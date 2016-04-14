9e1281548f4896cdf44a0bdad94f931ee6823c03
Abe Winter <awinter.public@gmail.com>
2016-04-13 23:27:22 -0400

    license & bump version

--- metamod/setup.py
+++ metamod/setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 ARGS = dict(
     name='metamod',
-    version='0.0.0', # todo: include git version and read VERSION from an init file
+    version='0.0.1',
     description='ORM that uses __slots__ (for memory saving), plain DBAPI2 connections (to not break threading) and assumes the user knows SQL',
     author='Abe Winter',
     author_email='internal-software@outlin.es',
     url='https://outlin.es',
-    license='INTERNAL USE ONLY. DO NOT PUBLISH, DO NOT SHARE',
+    license='MIT',
     packages=['metamod'],
     install_requires=['six', 'pytest'],
     entry_points={'console_scripts':[]},
